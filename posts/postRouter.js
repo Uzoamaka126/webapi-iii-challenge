27cffbe8cd9630563cd7bd9e5917d150ca6e8014
Uzoamaka126 <vanessapeters126@gmail.com>
2019-10-30 16:07:49 +0100

    First commit for this application

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,18 @@
 const express = 'express';
-
 const router = express.Router();
+const Posts = require('./postDb');
 
 router.get('/', (req, res) => {
-
+    Posts.get(req.query)
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'Error retrieving the hubs',
+            });
+        });
 });
 
 router.get('/:id', (req, res) => {
